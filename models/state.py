f897356ee28832df7e60aff096a96ecc3da740c1
Stewardvr93 <91085277+Stewardvr93@users.noreply.github.com>
2022-09-30 14:04:08 -0500

    correccion

--- models/state.py
+++ models/state.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, DateTime, Integer, String, ForeignKey
+from os import getenv
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import os
-from sqlalchemy.ext.declarative import declarative_base
-from models.city import City
-import models
 
 
-class State(BaseModel, Base):
+class State(BaseModel,
+            Base if (getenv("HBNB_TYPE_STORAGE") == "db") else object):
     """ State class """
-    __tablename__ = 'states'
-
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",  cascade="all, delete")
-
-    if "HBNB_TYPE_STORAGE" in os.environ \
-       and os.environ['HBNB_TYPE_STORAGE'] != "db":
+    if (getenv("HBNB_TYPE_STORAGE") == "db"):
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cites = relationship("City", backref="state", cascade="delete")
+    else:
+        name = ""
 
         @property
         def cities(self):
-            """
-                return the list of City objects
-                from storage linked to the current State
-            """
-            l = []
-            for k, v in models.storage.all(City).items():
-                if v.state_id == self.id:
-                    l.append(v)
-            return l
+            """ return a list of City instances"""
+            from models import storage
+            from models import city
+            my_list = []
+            for value in storage.all(city).values():
+                if value.__class__ == 'City' and self.id == value.state_id:
+                    my_list.append(value)
+            return my_list
